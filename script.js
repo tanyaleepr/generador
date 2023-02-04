85074c5b153e10c2086ea088963192d431997d3a
tanyaleepr <tanyaleepr@gmail.com>
2023-02-04 18:01:39 -0500

    added final touch for review on desktop, needs responsiveness and database

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function generateWord() {
-  const words = ['Hello', 'World', 'Goodbye', 'Friend'];
+  const words = ['Hello', 'World', 'Goodbyeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee', 'Friend'];
   const randomIndex = Math.floor(Math.random() * words.length);
   const randomWord = words[randomIndex];
   document.querySelector('#word').innerHTML = randomWord;
