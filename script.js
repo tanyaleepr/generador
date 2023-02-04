2028b6589e52835ae1fb361861b40c247637293e
tanyaleepr <tanyaleepr@gmail.com>
2023-02-04 18:10:32 -0500

    created creditos list

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function generateWord() {
-  const words = ['Hello', 'World', 'Goodbyeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee', 'Friend'];
+  const words = ['Hello', 'World', 'Quisiera ser agua para mojar tu jardín....', 'Friend'];
   const randomIndex = Math.floor(Math.random() * words.length);
   const randomWord = words[randomIndex];
   document.querySelector('#word').innerHTML = randomWord;
